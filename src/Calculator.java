a2571109ebaea63710db459c6c9701fef9dc82fa
somename <someemail>
2014-03-12 19:05:02 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 			String[] operands = expression.split("\\+");
 			return eval(operands[0])+eval(operands[1]);
 		}else if(expression.contains("-")){
-			String[] operands = expression.split("\\+");
+			String[] operands = expression.split("\\-");
 			return eval(operands[0])+eval(operands[1]);
 		} else {
 			return Double.parseDouble(expression);
