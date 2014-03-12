f5f56e665f2c178ab544146f72b0358128d09719
somename <someemail>
2014-03-12 18:59:50 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,10 @@
-
 public class Calculator {
 
 	public static double eval(String expression) {
 		if(expression.contains("+")){
-			
-			return Double.parseDouble(expression);
-		} catch (NumberFormatException e) {
 			return 2;
+		} else {
+			return Double.parseDouble(expression);
 		}
 	}
 
