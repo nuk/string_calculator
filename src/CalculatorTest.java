90833cd8cfadec163fdf17be527567cddf70ef58
somename <someemail>
2014-03-12 18:56:57 -0300

    besouro automatic message

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
 
 	@Test public void returnStringsWithOnlyTheNUmber(){
 		assertEquals(1.0, Calculator.eval("1"),0);
-		assertEquals(2.0, Calculator.eval("2"),0);
+		assertEquals(2.0, Calculator.eval("25"),0);
 	}
 	
 }
