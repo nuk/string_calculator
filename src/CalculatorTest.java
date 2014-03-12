2248086bf8374a2e170a0b17884023a36abd30bf
somename <someemail>
2014-03-12 18:57:35 -0300

    besouro automatic message

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -11,8 +11,7 @@ public class CalculatorTest {
 	}
 	
 	@Test public void itSums(){
-		assertEquals(1.0, Calculator.eval("1"),0);
-		assertEquals(25.0, Calculator.eval("25"),0);
+		assertEquals(1.0, Calculator.eval("1+1"),0);
 	}
 	
 }
