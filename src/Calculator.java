952c08b309b24c261c98d70dc4e72db2c3617e5c
somename <someemail>
2014-03-12 18:52:12 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@
 public class Calculator {
 
 	public static double eval(String string) {
-		return 0;
+		return 1;
 	}
 
 }
--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test public void returnStringsWithOnlyTheNUmber(){
-		assertEquals(1.0, Calculator.eval("1"));
+		assertEquals(1.0, Calculator.eval("1"),0);
 	}
 	
 }
