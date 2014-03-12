0720f2d96cd10cf8aa76c1c6169064c657999106
somename <someemail>
2014-03-12 19:13:57 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -29,6 +29,10 @@ abstract class BinaryExpression{
 
 class Sum extends BinaryExpression{
 	public Sum() {
-		
+		symbol = "+";
+	}
+	@Override
+	float eval(float op1, float op2) {
+		return op1 +op2;
 	}
 }
