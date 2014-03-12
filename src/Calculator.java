6e03f14884495257574d8da8b3ae22b7e79940e7
somename <someemail>
2014-03-12 19:00:53 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,7 @@ public class Calculator {
 
 	public static double eval(String expression) {
 		if(expression.contains("+")){
+			expression.split("+")
 			return 2;
 		} else {
 			return Double.parseDouble(expression);
