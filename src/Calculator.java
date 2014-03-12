4760a8e7de1f75a7f6cf3ecae8dd028fa835353d
somename <someemail>
2014-03-12 19:10:34 -0300

    besouro automatic message

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,9 +10,9 @@ public class Calculator {
 		}else if(expression.contains("*")){
 			String[] operands = expression.split("\\*");
 			return eval(operands[0])*eval(operands[1]);
-		}else if(expression.contains("*")){
-			String[] operands = expression.split("\\*");
-			return eval(operands[0])*eval(operands[1]);
+		}else if(expression.contains("/")){
+			String[] operands = expression.split("\\/");
+			return eval(operands[0])/eval(operands[1]);
 		} else {
 			return Double.parseDouble(expression);
 		}
